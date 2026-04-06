dab65fb6b088753fb579dea88e9d98aa6c073afe
QWQ2025 <90580525+QWQ2025QWQ@users.noreply.github.com>
2026-04-06 17:56:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/derisamedia/luci-theme-alpha package/luci-theme-alpha
+git clone https://github.com/derisamedia/luci-app-arwi-dashboard package/luci-app-arwi-dashboard
